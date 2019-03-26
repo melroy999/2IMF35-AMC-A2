cb4e8e411d46a450e6d35bfea5a9e61cf0c904ab
Melroy <m.v.nijnatten@student.tue.nl>
2019-03-26 12:26:02 +0100

    [/master] Ran strategies 2, 3, 4, 5, 6 and 7 with 10 repetitions.

--- test_cache_misses.sh
+++ test_cache_misses.sh
@@ -2,11 +2,11 @@ cd out
 
 for i in ./inputs/experiment1/* ./inputs/experiment2/* ./inputs/experiment3/*
 do
-	perf stat -e L1-dcache-loads,L1-dcache-load-misses,dTLB-loads,dTLB-load-misses,cache-misses java -Xmx4g -cp . s2imf35.Main -game=$i -linear -strategy=2 > /dev/null
+	perf stat -e L1-dcache-loads,L1-dcache-load-misses,dTLB-loads,dTLB-load-misses,cache-misses java -Xmx4g -cp . s2imf35.Main -game=$i -linear -strategy=2 -timeout=30 > /dev/null
 
-	perf stat -e L1-dcache-loads,L1-dcache-load-misses,dTLB-loads,dTLB-load-misses,cache-misses java -Xmx4g -cp . s2imf35.Main -game=$i -linear -strategy=3 > /dev/null
+	perf stat -e L1-dcache-loads,L1-dcache-load-misses,dTLB-loads,dTLB-load-misses,cache-misses java -Xmx4g -cp . s2imf35.Main -game=$i -linear -strategy=3 -timeout=30 > /dev/null
 
-	perf stat -e L1-dcache-loads,L1-dcache-load-misses,dTLB-loads,dTLB-load-misses,cache-misses java -Xmx4g -cp . s2imf35.Main -game=$i -linear -strategy=4 > /dev/null
+	perf stat -e L1-dcache-loads,L1-dcache-load-misses,dTLB-loads,dTLB-load-misses,cache-misses java -Xmx4g -cp . s2imf35.Main -game=$i -linear -strategy=4 -timeout=30 > /dev/null
 
-	perf stat -e L1-dcache-loads,L1-dcache-load-misses,dTLB-loads,dTLB-load-misses,cache-misses java -Xmx4g -cp . s2imf35.Main -game=$i -linear -strategy=6 > /dev/null
+	perf stat -e L1-dcache-loads,L1-dcache-load-misses,dTLB-loads,dTLB-load-misses,cache-misses java -Xmx4g -cp . s2imf35.Main -game=$i -linear -strategy=6 -timeout=30 > /dev/null
 done
