f154214228be631ddc6d5703be8b32bad3a58322
= <m.v.nijnatten@student.tue.nl>
2019-03-29 15:25:28 +0000

    [/master] Added cache miss analysis for input order and random order strategies.

--- install_java_and_perf.sh
+++ install_java_and_perf.sh
@@ -1,6 +1,6 @@
 apt install default-jre
 apt install default-jdk
 apt install linux-tools-common
-apt install linux-tools-4.15.0-46-generic
+apt install linux-tools-4.15.0-29-generic
 apt install linux-tools-generic
 
--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,14 +1,8 @@
 cd out
 mkdir logs
-touch logs/log2.txt
+#touch logs/log0.txt
+#touch logs/log1.txt
 touch logs/log3.txt
-touch logs/log4.txt
-touch logs/log5.txt
-touch logs/log6.txt
-touch logs/log7.txt
-java -Xmx4g -Xms4g -cp . s2imf35.Main -all -linear -strategy=2 -timeout=30 -n=10 > logs/log2.txt
+#java -Xmx4g -Xms4g -cp . s2imf35.Main -all -linear -strategy=0 -timeout=30 -n=10 > logs/log0.txt
+#java -Xmx4g -Xms4g -cp . s2imf35.Main -all -linear -strategy=1 -timeout=30 -n=10 > logs/log1.txt
 java -Xmx4g -Xms4g -cp . s2imf35.Main -all -linear -strategy=3 -timeout=30 -n=10 > logs/log3.txt
-java -Xmx4g -Xms4g -cp . s2imf35.Main -all -linear -strategy=4 -timeout=30 -n=10 > logs/log4.txt
-java -Xmx4g -Xms4g -cp . s2imf35.Main -all -linear -strategy=5 -timeout=30 -n=10 > logs/log5.txt
-java -Xmx4g -Xms4g -cp . s2imf35.Main -all -linear -strategy=6 -timeout=30 -n=10 > logs/log6.txt
-java -Xmx4g -Xms4g -cp . s2imf35.Main -all -linear -strategy=7 -timeout=30 -n=10 > logs/log7.txt
