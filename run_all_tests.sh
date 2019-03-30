4c51255d2eb1f4e6f340e51e78422b330fe644a6
Melroy <m.v.nijnatten@student.tue.nl>
2019-03-30 11:29:14 +0000

    [/master] Added 10 repetitions for strategies 0 and 1.

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,8 +1,8 @@
 cd out
 mkdir logs
-#touch logs/log0.txt
-#touch logs/log1.txt
-touch logs/log3.txt
-#java -Xmx4g -Xms4g -cp . s2imf35.Main -all -linear -strategy=0 -timeout=30 -n=10 > logs/log0.txt
-#java -Xmx4g -Xms4g -cp . s2imf35.Main -all -linear -strategy=1 -timeout=30 -n=10 > logs/log1.txt
-java -Xmx4g -Xms4g -cp . s2imf35.Main -all -linear -strategy=3 -timeout=30 -n=10 > logs/log3.txt
+touch logs/log0.txt
+touch logs/log1.txt
+#touch logs/log3.txt
+java -Xmx4g -Xms4g -cp . s2imf35.Main -all -linear -strategy=0 -timeout=30 -n=10 > logs/log0.txt
+java -Xmx4g -Xms4g -cp . s2imf35.Main -all -linear -strategy=1 -timeout=30 -n=10 > logs/log1.txt
+#java -Xmx4g -Xms4g -cp . s2imf35.Main -all -linear -strategy=3 -timeout=30 -n=10 > logs/log3.txt
